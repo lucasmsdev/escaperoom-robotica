3969fe1c760660a5d96d0a22cf3f3c2b3e668a2c
lucasmsdev <lucasdevoficial@gmail.com>
2023-11-22 10:06:30 -0300

    podendo organizar os enigmas porem nao podendo finalizar

--- src/app/pages/groupsix/groupsix.component.ts
+++ src/app/pages/groupsix/groupsix.component.ts
@@ -1,31 +1,70 @@
+// groupsix.component.ts
+
 import { Component, OnInit } from "@angular/core";
 import { BookService } from "src/app/services/book.service";
-import { Book } from "src/app/models/book.model";
 
 @Component({
-    selector: 'app-groupsix',
-    templateUrl: 'groupsix.component.html',
-    styleUrls: ['./groupsix.component.css']
+  selector: 'app-groupsix',
+  templateUrl: 'groupsix.component.html',
+  styleUrls: ['./groupsix.component.css']
 })
 
 export class GroupsixComponent implements OnInit {
-    
-    enigmas: string[] = [];
+  enigmas: string[] = [];
+  selectedEnigmaIndex: number | null = null;
 
-    constructor(private bookService: BookService) {}
-  
-    ngOnInit(): void {
-      // Obter todos os enigmas
-      this.enigmas = this.bookService.getAllEnigmas() || [];
-      
-      // Embaralhar a lista de enigmas
-      this.shuffleEnigmas();
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    // Obter todos os enigmas
+    const originalEnigmas = this.bookService.getAllEnigmas() || [];
+
+    // Embaralhar os enigmas
+    this.enigmas = this.shuffleEnigmas(originalEnigmas);
+
+    // Inicializar o índice do enigma selecionado como nulo
+    this.selectedEnigmaIndex = null;
+  }
+
+  selectEnigma(index: number): void {
+    // Marcar o enigma selecionado
+    this.selectedEnigmaIndex = index;
+  }
+
+  moveEnigmaUp(): void {
+    // Mover o enigma selecionado para cima na lista
+    if (this.selectedEnigmaIndex !== null && this.selectedEnigmaIndex > 0) {
+      this.swapEnigmas(this.selectedEnigmaIndex, this.selectedEnigmaIndex - 1);
     }
-  
-    private shuffleEnigmas(): void {
-      for (let i = this.enigmas.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.enigmas[i], this.enigmas[j]] = [this.enigmas[j], this.enigmas[i]];
-      }
+  }
+
+  moveEnigmaDown(): void {
+    // Mover o enigma selecionado para baixo na lista
+    if (
+      this.selectedEnigmaIndex !== null &&
+      this.selectedEnigmaIndex < this.enigmas.length - 1
+    ) {
+      this.swapEnigmas(this.selectedEnigmaIndex, this.selectedEnigmaIndex + 1);
     }
-}   
+  }
+
+  private shuffleEnigmas(enigmas: string[]): string[] {
+    // Embaralhar os enigmas utilizando o algoritmo Fisher-Yates
+    const shuffledEnigmas = [...enigmas];
+    for (let i = shuffledEnigmas.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledEnigmas[i], shuffledEnigmas[j]] = [shuffledEnigmas[j], shuffledEnigmas[i]];
+    }
+    return shuffledEnigmas;
+  }
+
+  private swapEnigmas(indexA: number, indexB: number): void {
+    // Trocar os enigmas nas posições indexA e indexB
+    const temp = this.enigmas[indexA];
+    this.enigmas[indexA] = this.enigmas[indexB];
+    this.enigmas[indexB] = temp;
+
+    // Atualizar o índice do enigma selecionado após a troca
+    this.selectedEnigmaIndex = indexB;
+  }
+}
